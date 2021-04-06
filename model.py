743461e86d54bd68430f1493baf52452d89b360f
mohamed244134 <mohamedchoots@gmail.com>
2021-04-06 21:24:41 +0000

    adding model file which contains the model

---
+++ model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from keras.layers import Dense, Flatten, Lambda, Activation, MaxPooling2D
+
+from keras.layers.convolutional import Convolution2D
+
+from keras.models import Sequentioal
+
+from keras.optimizers import Adam
+
